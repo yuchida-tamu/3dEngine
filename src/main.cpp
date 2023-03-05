9075c1046d017ea3ebc85f16dec4a046d3d3d710
Yuta Uchida <yuchida4dev@gmail.com>
2023-03-06 00:04:42 +0900

    refactor mian

--- src/main.cpp
+++ src/main.cpp
@@ -143,22 +143,143 @@ float lightCubeVertices[] = {
     -0.5f,
 };
 
-void CreateShaders()
+void create_shaders();
+void create_objects();
+void render_meshes();
+void processInput(GLFWwindow *window, CameraObject *camera);
+void mouse_callback(GLFWwindow *window, double xposIn, double yposIn);
+
+int main()
+{
+    mainWindow = Window();
+    mainWindow.Initialize();
+    mainCamera = new CameraObject();
+    glfwSetCursorPosCallback(mainWindow.GetWindow(), mouse_callback);
+
+    create_objects();
+
+    // Light Object
+    unsigned int lightVAO, lightVBO;
+    glGenVertexArrays(1, &lightVAO);
+    glGenBuffers(1, &lightVBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(lightCubeVertices), lightCubeVertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(lightVAO);
+     // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // build and compile our shader program
+    create_shaders();
+
+    Shader lightingShader = Shader("src/shaders/lighting.vs", "src/shaders/lighting.fs");
+
+    glEnable(GL_DEPTH_TEST);
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    while (!mainWindow.GetShouldClose())
+    {
+
+        // input
+        processInput(mainWindow.GetWindow(), mainCamera);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shaderList[0].UseShader();
+
+        glm::mat4 view = mainCamera->GetViewMatrix();
+        int viewLocation = shaderList[0].GetUniformView();
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+
+        render_meshes();
+
+        // render lighting object
+        lightingShader.UseShader();
+        lightingShader.SetUniformVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
+        lightingShader.SetUniformVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+
+        glm::mat4 lightView = mainCamera->GetViewMatrix();
+        int lightViewLocation = lightingShader.GetUniformView();
+        glUniformMatrix4fv(lightViewLocation, 1, GL_FALSE, glm::value_ptr(lightView));
+        glm::mat4 lightModel = glm::mat4(1.0f);
+        lightModel = glm::translate(lightModel, glm::vec3(1.5f, 0.0f, -5.0f));
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        int projectLocation = lightingShader.GetUniformProjection();
+        int lightModelLocation = lightingShader.GetUniformModel();
+        glUniformMatrix4fv(projectLocation, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(lightModelLocation, 1, GL_FALSE, glm::value_ptr(lightModel));
+        // render the cube
+        glBindVertexArray(lightVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        mainWindow.SwapBuffers();
+
+        glfwPollEvents();
+    }
+
+    glUseProgram(0);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window, CameraObject *camera)
+{
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera->ProcessInput(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera->ProcessInput(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera->ProcessInput(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera->ProcessInput(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow *window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    xoffset = xpos - lastX;
+    yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    mainCamera->ProcessMouseMovement(xoffset, yoffset);
+}
+
+void create_shaders()
 {
     Shader *shader1 = new Shader("src/shaders/shader.vert", "src/shaders/shader.frag");
     shaderList.push_back(*shader1);
 }
 
-void CreateObjects()
-{
-    // float vertices[] = {
-    //     // positions        //colors
-    //     0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
-    //     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-    //     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-    //     -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,  // top left
-    // };
 
+void create_objects()
+{
     float vertices[] = {
         // positions        // texture
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
@@ -213,7 +334,7 @@ void CreateObjects()
     meshList.push_back(obj1);
 }
 
-void RenderMeshes()
+void render_meshes()
 {
     for (Mesh *mesh : meshList)
     {
@@ -229,126 +350,4 @@ void RenderMeshes()
 
         mesh->RenderMesh();
     }
-}
-
-void processInput(GLFWwindow *window, CameraObject *camera)
-{
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera->ProcessInput(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera->ProcessInput(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera->ProcessInput(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera->ProcessInput(RIGHT, deltaTime);
-}
-
-void mouse_callback(GLFWwindow *window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    xoffset = xpos - lastX;
-    yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
-
-    mainCamera->ProcessMouseMovement(xoffset, yoffset);
-}
-
-int main()
-{
-    mainWindow = Window();
-    mainWindow.Initialize();
-    mainCamera = new CameraObject();
-    glfwSetCursorPosCallback(mainWindow.GetWindow(), mouse_callback);
-
-    CreateObjects();
-
-    // Light Object
-    unsigned int lightVAO, lightVBO;
-    glGenVertexArrays(1, &lightVAO);
-    glGenBuffers(1, &lightVBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(lightCubeVertices), lightCubeVertices, GL_STATIC_DRAW);
-
-    glBindVertexArray(lightVAO);
-     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    // build and compile our shader program
-    CreateShaders();
-
-    Shader lightingShader = Shader("src/shaders/lighting.vs", "src/shaders/lighting.fs");
-
-    glEnable(GL_DEPTH_TEST);
-
-    // uncomment this call to draw in wireframe polygons.
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // render loop
-    while (!mainWindow.GetShouldClose())
-    {
-
-        // input
-        processInput(mainWindow.GetWindow(), mainCamera);
-
-        // render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        shaderList[0].UseShader();
-
-        glm::mat4 view = mainCamera->GetViewMatrix();
-        int viewLocation = shaderList[0].GetUniformView();
-        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
-
-        RenderMeshes();
-
-        // render lighting object
-        lightingShader.UseShader();
-        lightingShader.SetUniformVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
-        lightingShader.SetUniformVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-
-        glm::mat4 lightView = mainCamera->GetViewMatrix();
-        int lightViewLocation = lightingShader.GetUniformView();
-        glUniformMatrix4fv(lightViewLocation, 1, GL_FALSE, glm::value_ptr(lightView));
-        glm::mat4 lightModel = glm::mat4(1.0f);
-        lightModel = glm::translate(lightModel, glm::vec3(1.5f, 0.0f, -5.0f));
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-        int projectLocation = lightingShader.GetUniformProjection();
-        int lightModelLocation = lightingShader.GetUniformModel();
-        glUniformMatrix4fv(projectLocation, 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(lightModelLocation, 1, GL_FALSE, glm::value_ptr(lightModel));
-        // render the cube
-        glBindVertexArray(lightVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        mainWindow.SwapBuffers();
-
-        glfwPollEvents();
-    }
-
-    glUseProgram(0);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    glfwTerminate();
-    return 0;
 }
